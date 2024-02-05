8303c988ce01352650c7a5d874de6e9813288adc
Harsh-2910 <harshpcm2910@gmail.com>
2024-02-06 00:36:38 +0530

    some changes in intern validator

--- src/lib/internvalidators.ts
+++ src/lib/internvalidators.ts
@@ -1,18 +1,19 @@
-import { z } from "zod";
+import { string, z } from "zod";
 import errorMap from "zod/locales/en.js";
 
 export const AuthCreadentialsValidate = z.object({
   companyName: z.string().min(1,{message:"Mandatory field"}),
   companyLogo: z.string().url({message:"Enter company logo url"}),
   internshipTitle: z.string().min(1,{message:"Mandatory field"}),
-  qualification: z.string().min(1,{message:"Mandatory field"}),
+  qualification: z.string(),
   internshipDescription: z.string().min(1,{message:"Mandatory field"}),
   internshipLocation: z.string().min(1,{message:"Mandatory field"}),
   internshipApplyLink: z.string().url(),
   stipend: z.string(),
-  HrName: z.string().optional(),
-  HrContactNumber: z.string().optional(),
-  HrEmail: z.string().email().optional(),
+  hrName: z.string().optional(),
+  hrContactNumber: z.string().optional(),
+  hrEmail: z.string().email().optional(),
+  // category: z.string()
 });
 
 export type TAUTHCREDENTIALVALIDATER = z.infer<typeof AuthCreadentialsValidate>;
